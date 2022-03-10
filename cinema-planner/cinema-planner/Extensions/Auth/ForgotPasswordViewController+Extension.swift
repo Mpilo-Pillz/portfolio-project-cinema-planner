8d9bd9da3b2cfb79c55a01176fac2b0728f0dcad
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-03-10 13:12:45 +0200

    creating forgot password layout extension

--- cinema-planner/cinema-planner/Extensions/Auth/ForgotPasswordViewController+Extension.swift
+++ cinema-planner/cinema-planner/Extensions/Auth/ForgotPasswordViewController+Extension.swift
@@ -5,4 +5,18 @@
 //  Created by Mpilo Pillz on 2023/03/04.
 //
 
-import Foundation
+import UIKit
+
+extension ForgotPasswordViewController {
+    func layoutForgotPasswordViewController() {
+        forgotPasswordStackView.addArrangedSubview(currentPasswordTextField)
+        
+        view.addSubview(forgotPasswordStackView)
+        
+        NSLayoutConstraint.activate([
+            forgotPasswordStackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 2),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: forgotPasswordStackView.trailingAnchor, multiplier: 2),
+            forgotPasswordStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
