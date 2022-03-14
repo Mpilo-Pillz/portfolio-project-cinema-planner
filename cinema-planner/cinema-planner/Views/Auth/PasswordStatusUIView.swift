39627f372c9ab3975af8919f3d92cf9c345e8fde
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-03-14 12:30:45 +0200

    feat: WIP creating password status view

--- cinema-planner/cinema-planner/Views/Auth/PasswordStatusUIView.swift
+++ cinema-planner/cinema-planner/Views/Auth/PasswordStatusUIView.swift
@@ -8,13 +8,28 @@
 import UIKit
 
 class PasswordStatusUIView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+let passwordStatusStackView = makeStackView(withSpacing: 8, withAxis: .vertical)
+    
+    let criteriaLabel = UILabel()
+    
+    let lengthCriteriaView = PasswordCriteriaUIView(text: "8-32 characters (no spaces)")
+    let uppercaseCriteriaView = PasswordCriteriaUIView(text: "uppercase letter (A-Z)")
+    let lowercaseCriteriaView = PasswordCriteriaUIView(text: "lowercase (a-z)")
+    let specialCharacterCriteriaView = PasswordCriteriaUIView(text: "special character (e.g. !@#$%^)")
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
-    */
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder: has not been implemented")
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 200, height: 200)
+    }
+    
+   
 
 }
