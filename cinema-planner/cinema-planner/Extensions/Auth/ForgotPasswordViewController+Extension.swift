d810a291c301eeac2563107d80dad808f8a466c2
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-03-18 12:31:45 +0200

    feat: WIP adding styles for  view controller

--- cinema-planner/cinema-planner/Extensions/Auth/ForgotPasswordViewController+Extension.swift
+++ cinema-planner/cinema-planner/Extensions/Auth/ForgotPasswordViewController+Extension.swift
@@ -10,6 +10,7 @@ import UIKit
 extension ForgotPasswordViewController {
     func layoutForgotPasswordViewController() {
         forgotPasswordStackView.addArrangedSubview(currentPasswordTextField)
+        forgotPasswordStackView.addArrangedSubview(passwordStatusUIView)
         
         view.addSubview(forgotPasswordStackView)
         
@@ -18,5 +19,11 @@ extension ForgotPasswordViewController {
             view.trailingAnchor.constraint(equalToSystemSpacingAfter: forgotPasswordStackView.trailingAnchor, multiplier: 2),
             forgotPasswordStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
+    }
+    
+    func styleForgotPasswordViewController() {
+        passwordStatusUIView.translatesAutoresizingMaskIntoConstraints = false
+        passwordStatusUIView.layer.cornerRadius = 5
+        passwordStatusUIView.clipsToBounds = true
     }
 }
