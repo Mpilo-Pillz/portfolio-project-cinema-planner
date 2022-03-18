8dde3340d47e746cb5c0b7c76eab5c134741d07a
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-03-18 12:32:45 +0200

    feat: WIP adding styles for  view controller

--- cinema-planner/cinema-planner/Extensions/Auth/PasswordStatusUIView+Extension.swift
+++ cinema-planner/cinema-planner/Extensions/Auth/PasswordStatusUIView+Extension.swift
@@ -5,4 +5,41 @@
 //  Created by Mpilo Pillz on 2023/03/18.
 //
 
-import Foundation
+import UIKit
+
+extension PasswordStatusUIView {
+    
+    func passwordStatusUIViewLayout() {
+        passwordStatusStackView.distribution = .equalCentering
+        
+        passwordStatusStackView.addArrangedSubview(lengthCriteriaView)
+        passwordStatusStackView.addArrangedSubview(criteriaLabel)
+        passwordStatusStackView.addArrangedSubview(uppercaseCriteriaView)
+        passwordStatusStackView.addArrangedSubview(lowercaseCriteriaView)
+        passwordStatusStackView.addArrangedSubview(digitCriteriaView)
+        passwordStatusStackView.addArrangedSubview(specialCharacterCriteriaView)
+        addSubview(passwordStatusStackView)
+        
+        NSLayoutConstraint.activate([
+            passwordStatusStackView.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 2),
+            passwordStatusStackView.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 2),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: passwordStatusStackView.trailingAnchor, multiplier: 2),
+            bottomAnchor.constraint(equalToSystemSpacingBelow: passwordStatusStackView.bottomAnchor, multiplier: 2)
+        ])
+    }
+    private func makeCriteriaMessage() -> NSAttributedString {
+        var plainTextAttributes = [NSAttributedString.Key: AnyObject]()
+        plainTextAttributes[.font] = UIFont.preferredFont(forTextStyle: .subheadline)
+        plainTextAttributes[.foregroundColor] = UIColor.secondaryLabel
+        
+        var boldTextAttributes = [NSAttributedString.Key: AnyObject]()
+        boldTextAttributes[.foregroundColor] = UIColor.label
+        boldTextAttributes[.font] = UIFont.preferredFont(forTextStyle: .subheadline)
+
+        let attrText = NSMutableAttributedString(string: "Use at least ", attributes: plainTextAttributes)
+        attrText.append(NSAttributedString(string: "3 of these 4 ", attributes: boldTextAttributes))
+        attrText.append(NSAttributedString(string: "criteria when setting your password:", attributes: plainTextAttributes))
+
+        return attrText
+    }
+}
