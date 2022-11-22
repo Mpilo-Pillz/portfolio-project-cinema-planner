3c16e11639872e45714fc01862395e5c97ac3307
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-11-22 20:38:04 +0200

    feat: making serach tiems tappable to show preview

--- cinema-planner/cinema-planner/Controllers/SearchViewController.swift
+++ cinema-planner/cinema-planner/Controllers/SearchViewController.swift
@@ -112,7 +112,7 @@ extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
     }
 }
 
-extension SearchViewController:  UISearchResultsUpdating {
+extension SearchViewController:  UISearchResultsUpdating, SearchResultsViewControllerDelegate {
     func updateSearchResults(for searchController: UISearchController) {
         let searchBar = searchController.searchBar
         
@@ -122,6 +122,9 @@ extension SearchViewController:  UISearchResultsUpdating {
               let resultsController = searchController.searchResultsController as? SearchResultsViewController else {
             return
         }
+        
+        resultsController.delegate = self
+        
         APICaller.shared.search(with: query) { result in
             DispatchQueue.main.async {
                 switch result {
@@ -132,6 +135,15 @@ extension SearchViewController:  UISearchResultsUpdating {
                     print(error.localizedDescription)
                 }
             }
+        }
+    }
+    
+    func searchResultsViewControllerDidTapItem(_ viewModel: TitlePreviewViewModel) {
+        
+        DispatchQueue.main.async { [weak self] in
+            let vc = TitlePreviewViewController()
+            vc.configure(with: viewModel)
+            self?.navigationController?.pushViewController(vc, animated: true)
         }
     }
 }
