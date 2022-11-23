f65f22c2149960a70ceeed3ad403478f93f31a8e
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-11-23 18:06:58 +0200

    feat: core data - configuring core data

--- cinema-planner/cinema-planner/Controllers/DownloadsViewController.swift
+++ cinema-planner/cinema-planner/Controllers/DownloadsViewController.swift
@@ -8,14 +8,43 @@
 import UIKit
 
 class DownloadsViewController: UIViewController {
+    
+    private var titles: [TitleItem] = [TitleItem]()
 
+    private let downloadedTable: UITableView = {
+        let table = UITableView()
+        table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        title = "Downloads"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        downloadedTable.delegate = self
+        downloadedTable.dataSource = self
         
     }
-    
+}
 
+extension DownloadsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
     
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        let title = titles[indexPath.row]
+        cell.configure(with: TitleViewModel(titleName: (title.original_title ?? title.original_name) ?? "Untitled Video", posterURL: title.poster_path ?? "https://picsum.photos/200/300"))
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 180
+    }
 }
