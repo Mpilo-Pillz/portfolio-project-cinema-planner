82813b0386ff7c2ac450a9ffc6890259f04fb913
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-11-23 18:32:49 +0200

    successfully saving downloads to DB

--- cinema-planner/cinema-planner/Managers/DataPersistenceManager.swift
+++ cinema-planner/cinema-planner/Managers/DataPersistenceManager.swift
@@ -12,6 +12,7 @@ class DataPersistenceManager {
     
     enum DatabaseError: Error {
         case failedToSaveData
+        case failedToFetchData
     }
     static let shared = DataPersistenceManager()
     
@@ -41,7 +42,28 @@ class DataPersistenceManager {
         } catch {
             completion(.failure(DatabaseError.failedToSaveData))
         }
+    
         
+    }
+    func fetchingTitlesFromDataBase(completion: @escaping (Result<[TitleItem], Error>) -> Void) {
         
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request: NSFetchRequest<TitleItem>
+        
+        request = TitleItem.fetchRequest()
+        
+        do {
+            
+            let titles = try context.fetch(request)
+            completion(.success(titles))
+            
+        } catch {
+            completion(.failure(DatabaseError.failedToFetchData))
+        }
     }
 }
