92bf4e770e3a2b91530106aeec449c94d67ab3c9
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-11-25 07:25:14 +0200

    WIP - adding incomplete view controller, app is currently crashing

---
+++ cinema-planner/cinema-planner/Extensions/OnboardingViewController+Extension.swift
@@ -0,0 +1,43 @@
+//
+//  OnboardingViewController+Extension.swift
+//  cinema-planner
+//
+//  Created by Mpilo Pillz on 2022/11/25.
+//
+
+import UIKit
+
+extension OnboardingViewController {
+    func style() {
+        view.backgroundColor = .systemBackground
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: heroImageName)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        label.adjustsFontForContentSizeCategory = true
+        label.numberOfLines = 0
+        label.text = titleText
+    }
+    
+    func layout() {
+        stackView.addArrangedSubview(imageView)
+        stackView.addArrangedSubview(label)
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1)
+        ])
+    }
+}
