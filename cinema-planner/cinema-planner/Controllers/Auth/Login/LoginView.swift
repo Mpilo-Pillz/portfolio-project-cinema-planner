b72256568d07e3677e45ebac335ee4deb387fdfc
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-11-26 21:35:51 +0200

    feat: creating loginview and controller

--- cinema-planner/cinema-planner/Controllers/Auth/Login/LoginView.swift
+++ cinema-planner/cinema-planner/Controllers/Auth/Login/LoginView.swift
@@ -5,4 +5,42 @@
 //  Created by Mpilo Pillz on 2022/11/26.
 //
 
-import Foundation
+import UIKit
+
+class LoginView: UIView {
+    
+    let loginViewStackView = makeStackView(withSpacing: 8, withAxis: .vertical)
+    let loginViewUsernameTextField = makeTextField(withPlaceholder: "Username", isSecureTextEntry: false)
+    let loginViewPasswordTextField = makeTextField(withPlaceholder: "Password", isSecureTextEntry: true)
+    let LoginViewDividerView = UIView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        styleLoginView()
+        layoutLoginView()
+        setupLoginViewConstraints()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension LoginView {
+    private func styleLoginView() {
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .secondarySystemBackground
+    }
+    
+    private func layoutLoginView() {
+        loginViewStackView.addArrangedSubview(loginViewUsernameTextField)
+        loginViewStackView.addArrangedSubview(LoginViewDividerView)
+        loginViewStackView.addArrangedSubview(loginViewPasswordTextField)
+    }
+    
+    private func setupLoginViewConstraints() {
+        
+    }
+}
+
--- cinema-planner/cinema-planner/Controllers/Auth/Login/LoginViewController.swift
+++ cinema-planner/cinema-planner/Controllers/Auth/Login/LoginViewController.swift
@@ -7,23 +7,55 @@
 
 import UIKit
 
+protocol LogoutDelegate: AnyObject {
+    func didLogout()
+}
+
+protocol LoginViewControllerDelegate: AnyObject {
+    func didLogin()
+}
+
 class LoginViewController: UIViewController {
+    
+    let titleLabel = makeLable(withText: "Cinema Planner", alignment: .center, forTextStyle: .largeTitle)
+    let subTitleLabel = makeLable(withText: "Never miss a movie premier again!", alignment: .center, forTextStyle: .title3)
+    
+    let loginView = LoginView()
+    var signInButton = makePrimaryButton(withText: "Sign In")
+    let errorMessageLabel = UILabel()
+    
+    weak var delegate: LoginViewControllerDelegate?
+    
+    var username: String? {
+        return loginView.loginViewUsernameTextField.text
+    }
+    
+    var password: String? {
+        return loginView.loginViewPasswordTextField.text
+    }
+    
+    var leadingEdgeOnScreen: CGFloat = 16
+    var leadingEdgeOffScreen: CGFloat = -1000
+    
+    var titleLeadingAnchor: NSLayoutConstraint?
+    var subtitleLeadingAnchor: NSLayoutConstraint?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        style()
+        layout()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        signInButton.configuration?.showsActivityIndicator = false
     }
-    */
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        animate()
+    }
+
+  
 
 }
