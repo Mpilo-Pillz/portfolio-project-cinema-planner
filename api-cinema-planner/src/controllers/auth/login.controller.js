dc2c25dfd63de859858544bc8ab498676313665e
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-12-02 20:26:07 +0200

    feat: auth - creating hard coded login route

---
+++ api-cinema-planner/src/controllers/auth/login.controller.js
@@ -0,0 +1,16 @@
+exports.login =  (req, res) => {
+    const {email, password } = req.body
+
+    let existingUser = {
+        email: "thulani@karabo.co.za", 
+        password: "Thapelo"
+    };
+
+    try {
+        if (existingUser.email === email && existingUser.password === password ) {
+            res.json({message: "You are logged in", status: "Authenticated"});
+        }
+    } catch (err) {
+        
+    }
+  };
