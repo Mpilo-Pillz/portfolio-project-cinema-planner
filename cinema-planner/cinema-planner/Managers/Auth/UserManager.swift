9fbd27b77cca21537d1733c1ebe19e579946f758
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-12-03 23:11:28 +0200

    preparing to make login request through user manager

--- cinema-planner/cinema-planner/Managers/Auth/UserManager.swift
+++ cinema-planner/cinema-planner/Managers/Auth/UserManager.swift
@@ -11,6 +11,8 @@ protocol UserManageable: AnyObject {
     func login(withEmail email: String, withPassword password: String, completion: @escaping(Result<User, NetworkError>) -> Void)
 }
 
+
+
 enum NetworkError: Error {
     case serverError
     case decodingError
@@ -22,5 +24,77 @@ struct User: Codable {
 }
 
 class UserManager: UserManageable {
-    
+    func login(withEmail email: String, withPassword password: String, completion: @escaping (Result<User, NetworkError>) -> Void) {
+        print("in the user")
+        guard let url = URL(string: "\(Constants.localbaseURL)/api/v1/auth/login") else {
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let bodyData = try? JSONSerialization.data(withJSONObject: [
+            "email": "thulani@karabo.co.za",
+        "password": "Thapelo"
+        ], options: .fragmentsAllowed)
+        
+        request.httpBody = bodyData
+        
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            DispatchQueue.main.async {
+                guard let data = data, error == nil else {
+                    return
+                }
+                
+                do {
+                    let user = try JSONDecoder().decode(User.self, from: data)
+                    completion(.success(user))
+                    print("user: \(user)")
+                } catch {
+                    completion(.failure(.decodingError))
+                    
+                }
+            }
+        }.resume()
+    }
 }
+
+
+//class UserManager: UserManageable {
+//    func login(withEmail email: String, withPassword password: String, completion: @escaping (Result<User, NetworkError>) -> Void) {
+//        print("in the user")
+//        guard let url = URL(string: "\(Constants.localbaseURL)/api/v1/auth/login") else {
+//            return
+//        }
+//
+//        var request = URLRequest(url: url)
+//
+//        let bodyData = try? JSONSerialization.data(withJSONObject: [
+//            "email": "thulani@karabo.co.za",
+//        "password": "Thapelo"
+//        ])
+//
+//        request.httpMethod = "POST"
+//        request.httpBody = bodyData
+//
+//        URLSession.shared.dataTask(with: url) { data, response, error in
+//            DispatchQueue.main.async {
+//                guard let data = data, error == nil else {
+//                    return
+//                }
+//
+//                do {
+//                    let user = try JSONDecoder().decode(User.self, from: data)
+//                    completion(.success(user))
+//                    print("user: \(user)")
+//                } catch {
+//                    completion(.failure(.decodingError))
+//
+//                }
+//            }
+//        }.resume()
+//    }
+//}
