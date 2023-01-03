99a42e2ec037e0d4b92d74dcb081b5b503100b74
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-01-03 21:21:02 +0200

    importing access secret

--- api-cinema-planner/src/controllers/youtubeController.js
+++ api-cinema-planner/src/controllers/youtubeController.js
@@ -1,6 +1,9 @@
-const youtubeBaseURL = "https://youtube.googleapis.com/youtube/v3"
+const { accessSecret } = require("../utils/gcp");
+
+const youtubeBaseURL = "https://youtube.googleapis.com/youtube/v3";
 
 exports.searchTrailers = async (req, res) => {
+    
     const response = await fetch(`${youtubeBaseURL}/search?q=${req.query.query}&key=${process.env.YOU_TUBE_API_KEY}`)
     const searchResults = await response.json();
   
