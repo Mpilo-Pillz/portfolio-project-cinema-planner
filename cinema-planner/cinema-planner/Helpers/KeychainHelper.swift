21d60fce0c7d1f38a3ff2fc5ce63f5444dafc9fa
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-01-16 21:42:24 +0200

    creating key chain helper

---
+++ cinema-planner/cinema-planner/Helpers/KeychainHelper.swift
@@ -0,0 +1,8 @@
+//
+//  KeychainHelper.swift
+//  cinema-planner
+//
+//  Created by Mpilo Pillz on 2023/01/15.
+//
+
+import Foundation
---
+++ cinema-planner/cinema-planner/Managers/Auth/AuthManager.swift
@@ -0,0 +1,8 @@
+//
+//  AuthManager.swift
+//  cinema-planner
+//
+//  Created by Mpilo Pillz on 2023/01/15.
+//
+
+import Foundation
