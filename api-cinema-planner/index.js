75e8dadbd0f17bc08ff860c35caf9766529f9493
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-01-21 15:33:35 +0200

    checking if connection to mongo works before starting app

--- api-cinema-planner/index.js
+++ api-cinema-planner/index.js
@@ -1,8 +1,14 @@
+require("dotenv").config();
 const http = require("http");
 const app = require("./app");
+const mongoose = require("mongoose");
+
 const port = process.env.PORT || 4000;
 
 app.set("port", port);
 const server = http.createServer(app);
 
-server.listen(port);
+mongoose
+  .connect(process.env.DB_CONNECTION_STRING)
+  .then(() => server.listen(port))
+  .catch((err) => console.log(err));
