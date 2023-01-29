285e09272277c020c7ee5b156fb5610f2b18b006
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-01-29 16:12:58 +0200

    creating profile settings page

---
+++ cinema-planner/cinema-planner/Controllers/Profile/ProfileSettingsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileSettingsViewController.swift
+//  cinema-planner
+//
+//  Created by Mpilo Pillz on 2023/01/29.
+//
+
+import UIKit
+
+class ProfileSettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
