c1a5f58ac41b1997b2cd0244471f2191e120ae9b
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-04-01 12:20:50 +0200

    adding reset password controller to routes

--- api-cinema-planner/src/routes/auth/userRoutes.js
+++ api-cinema-planner/src/routes/auth/userRoutes.js
@@ -1,9 +1,13 @@
-const Router = require('express');
-const { login, signup } = require("../../controllers/auth/loginController")
+const Router = require("express");
+const { login, signup } = require("../../controllers/auth/loginController");
+const {
+  resetPassword,
+} = require("../../controllers/auth/resetPasswordController");
 
 const userRoutes = Router();
 
-userRoutes.post("/login", login)
-userRoutes.post("/signup", signup)
+userRoutes.post("/login", login);
+userRoutes.post("/signup", signup);
+userRoutes.post("/resetPassword", resetPassword);
 
-module.exports = userRoutes
+module.exports = userRoutes;
